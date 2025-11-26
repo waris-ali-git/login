82bd236cfafb9ab2d57ba281e299838ab2551b4f
waris-ali-git <wa363376@gmail.com>
2025-11-27 01:17:32 +0500

    test file

---
+++ lib/sign_up.dart
@@ -0,0 +1,166 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final firstNameC = TextEditingController();
+  final lastNameC = TextEditingController();
+  final emailC = TextEditingController();
+  final passC = TextEditingController();
+  final confirmPassC = TextEditingController();
+
+  bool loading = false;
+
+  signup() async {
+    if (passC.text.trim() != confirmPassC.text.trim()) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Passwords do NOT match")));
+      return;
+    }
+
+    try {
+      setState(() => loading = true);
+
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailC.text.trim(),
+        password: passC.text.trim(),
+      );
+
+      // OPTIONAL → Save name to FirebaseAuth displayName
+      await FirebaseAuth.instance.currentUser?.updateDisplayName(
+        "${firstNameC.text.trim()} ${lastNameC.text.trim()}",
+      );
+
+      Navigator.pushReplacementNamed(context, '/student-dashboard');
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Signup Failed: $e")));
+    }
+
+    setState(() => loading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Create Account",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+
+                // FIRST NAME
+                TextField(
+                  controller: firstNameC,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueAccent,
+                    hintText: "First Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // LAST NAME
+                TextField(
+                  controller: lastNameC,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueAccent,
+                    hintText: "Last Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // EMAIL
+                TextField(
+                  controller: emailC,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueAccent,
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // PASSWORD
+                TextField(
+                  controller: passC,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueAccent,
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // CONFIRM PASSWORD
+                TextField(
+                  controller: confirmPassC,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueAccent,
+                    hintText: "Confirm Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // BUTTON
+                ElevatedButton(
+                  onPressed: loading ? null : signup,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pinkAccent,
+                    minimumSize: const Size(double.infinity, 55),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: loading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text("Sign Up", style: TextStyle(fontSize: 18)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
