63c022d10eb409fba49b8f1236bad6f6fdc9d8ee
waris-ali-git <wa363376@gmail.com>
2025-11-27 18:52:16 +0500

    commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -166,7 +166,7 @@ class _LoginScreenState extends State<LoginScreen> {
       style: const TextStyle(color: Colors.black12),
       decoration: InputDecoration(
         hintText: hintText,
-        hintStyle: TextStyle(color: Colors.black12),
+        hintStyle: TextStyle(color: Colors.red),
         prefixIcon: Icon(Icons.lock, color: Colors.grey.shade600),
         filled: true,
         fillColor: Colors.white.withOpacity(0.95),
